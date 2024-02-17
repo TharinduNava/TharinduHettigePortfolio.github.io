89776280433998a5e574d05af39f70c2f06820df
Tharindu Hettige <90743227+TharinduNava@users.noreply.github.com>
2024-02-17 11:21:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,10 +25,4 @@ $(document).ready(function () {
 		easing: 'ease',
 		duration: 1800
 	});
-
-	// Add the provided script here
-	window.onload = function () {
-		// Reset the form fields when the page loads
-		document.getElementById("form").reset();
-	};
 });
